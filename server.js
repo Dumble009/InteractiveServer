a21ba0747c63c13c1761325d0754e0cc5d748b9b
Dumble009 <daichi19990821@gmail.com>
2022-07-18 13:40:14 +0900

    add echo to UnityClient

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const server = require("ws").Server;
 const s = new server({ port: 5001 });
 
+let unityClients = [];
+
 s.on("connection", (ws) => {
   ws.on("message", (message) => {
     console.log("Received: " + message);
 
-    if (message.toString() === "hello") {
-      console.log("return message");
-      ws.send("hello from server");
+    msgString = message.toString();
+    if (message.toString() === "Unity") {
+      unityClients.push(ws);
+      console.log("accept new UnityClient : count " + unityClients.length);
+    } else {
+      console.log("echo to UnityClients");
+      unityClients.forEach((unityWS) => {
+        unityWS.send(msgString);
+      });
+    }
+  });
+
+  ws.on("close", (message) => {
+    console.log("Close : " + message);
+    idx = unityClients.indexOf(ws);
+    if (idx >= 0) {
+      unityClients.splice(idx, 1);
+      console.log("remove closed UnityClient : count " + unityClients.length);
     }
   });
 });
